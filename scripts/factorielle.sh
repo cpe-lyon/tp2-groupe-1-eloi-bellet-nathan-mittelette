d86c7abfbd928f1fb9dba6cfc6a5e4832289ae69
Nathan Mittelette <nathan.mittelette@mastertp>
2020-02-18 11:23:21 +0100

    update TP2.md + comments

--- scripts/factorielle.sh
+++ scripts/factorielle.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
+# Gestion du cas ou il n'y a pas de paramètre pour la commaned
 if [ "$1" == "" ]; then
-    echo Veuillez saisir un paramêtre
+    echo Veuillez saisir un paramètre
     exit 1
 fi
 
+# On initialise le conteur à 1 et la factorielle à 1
 counter=$1
 factorial=1
+
+# Tant que le compteur est plus grand que 0 on multiplie 
+# factoriel par le compteur et on le décrémente
 while [ $counter -gt 0 ]
 do
    factorial=$(( $factorial * $counter ))
    counter=$(( $counter - 1 ))
 done
+
+# Affichage du résultat
 echo $factorial
--- scripts/plusOuMoins.sh
+++ scripts/plusOuMoins.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
+# On définit le nomber aléatoire enter 1000 et 1
 number=$((RANDOM % 1000 + 1))
-echo $number
 
+# Création d'une boucle infini tant que l'utilisateur ne trouve pas
 while [ 1 -eq 1 ]; do
+    # Récupération de la saisi utilisateur
     echo -n "Votre nombre : "
     read currentNumber
+    # Si jamais le nombre est plus petit on l'affiche
     if [ $currentNumber -gt $number ]; then
         echo "Plus petit !"
+    #Si jamais le nombre est plus grand on l'affiche
     elif [ $currentNumber -lt $number ]; then
         echo "Plus grand !"
+    # Si jamais le nombre est le même on l'affiche et on quitte la boucle
     else
         echo "Trouvé !"
         break
